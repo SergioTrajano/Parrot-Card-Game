4fd736479a9b91c5efca4e5593d6e178787cea67
Sergio Trajano <sergio.trajanosilva2001@gmail.com>
2022-04-09 10:46:58 -0300

    TOdos requisitos de 'CLique na carta'

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 let cartas = [
-    {gif: "gifs/bobrossparrot.gif", status: "virado"},
-    {gif: "gifs/explodyparrot.gif", status: "virado"},
-    {gif: "gifs/fiestaparrot.gif", status: "virado"},
-    {gif: "gifs/metalparrot.gif", status: "virado"},
-    {gif: "gifs/revertitparrot.gif", status: "virado"},
-    {gif: "gifs/tripletsparrot.gif", status: "virado"},
-    {gif: "gifs/unicornparrot.gif", status: "virado"},
+    {gif: "gifs/bobrossparrot.gif", tipo: "um"},
+    {gif: "gifs/explodyparrot.gif", tipo: "dois"},
+    {gif: "gifs/fiestaparrot.gif", tipo: "tres"},
+    {gif: "gifs/metalparrot.gif", tipo: "quatro"},
+    {gif: "gifs/revertitparrot.gif", tipo: "cinco"},
+    {gif: "gifs/tripletsparrot.gif", tipo: "seis"},
+    {gif: "gifs/unicornparrot.gif", tipo: "sete"},
 ]
+
 function selecionaQuantidade() {
     let quantidade;
     do {
@@ -35,10 +36,62 @@ function iniciarJogo() {
     lista.innerHTML = "";
     for (let i = 0; i < quantidade; i++) {
         lista.innerHTML += `
-        <li>
+        <li onclick="virarCarta(this)">
             <div class="frente"> <img src="front.png"></div>
             <div class="atras"> <img src="${list[i].gif}"></div>
         </li>`;
+    }
+}
+
+function virarCarta (elemento) {
+    elemento.querySelector("div:first-child").classList.add("selecionadoFrente");
+    elemento.classList.add("clickado");
+    elemento.querySelector("div:last-child").classList.add("selecionadoAtras");
+    let cartasSelecionadas = document.querySelectorAll(".clickado");
+    if (cartasSelecionadas.length == 2) {
+        if (cartasSelecionadas[0].innerHTML == cartasSelecionadas[1].innerHTML) {
+            confirmaPar(cartasSelecionadas);
+        }
+        else {
+            setTimeout(desviraPar, 1000, cartasSelecionadas);
+        }
+    }
+} 
+
+function confirmaPar(cartasSelecionadas) {
+    for (let i = 0; i < cartasSelecionadas.length; i++) {
+        cartasSelecionadas[i].classList.remove("clickado");
+        atualizaCartas();
+    }
+}
+
+function desviraPar(cartasSelecionadas) {
+    for (let i = 0; i < cartasSelecionadas.length; i++) {
+        cartasSelecionadas[i].classList.remove("clickado");
+        cartasSelecionadas[i].querySelector("div:first-child").classList.remove("selecionadoFrente");
+        cartasSelecionadas[i].querySelector("div:last-child").classList.remove("selecionadoAtras");
+    }
+}
+
+function atualizaCartas() {
+    const lista = document.querySelectorAll("li");
+    const cartinhas = document.querySelector("ul");
+    cartinhas.innerHTML = "";
+    for (let i = 0; i < lista.length; i++) {
+        if (cartinhas[i].classList.contains("virado")) {
+            cartinhas.innerHTML += `
+                <li">
+                    <div class="frente"> <img src="front.png"></div>
+                    <div class="atras"> <img src="${list[i].gif}"></div>
+                </li>`;
+        }
+        else {
+            cartinhas.innerHTML += `
+                <li onclick="virarCarta(this)">
+                    <div class="frente"> <img src="front.png"></div>
+                    <div class="atras"> <img src="${list[i].gif}"></div>
+                </li>`;
+        }
     }
 }
 
